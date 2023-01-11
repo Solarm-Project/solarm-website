0e8e5974358acb0caa4c44b77ff641455416df3e
Till Wegmueller <toasterson@gmail.com>
2023-01-11 18:32:59 +0100

    Adding gh pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://solarm-project.github.io',
+  base: '/solarm-website'
 });
